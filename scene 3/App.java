43df926a1e2ff177c9eade7771242c2f0744fc7d
ashishbommali <39550160+ashishbommali@users.noreply.github.com>
2019-11-25 16:39:16 +0530

    Add files via upload

---
+++ scene 3/App.java
@@ -0,0 +1,25 @@
+package com.noesisinformatica.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args )
+    {
+	try{
+	String myDriver = "org.gjt.mm.mysql.Driver";
+      String myUrl = "jdbc:mysql://localhost/test";
+      Class.forName(myDriver);
+      Connection conn = DriverManager.getConnection(myUrl, "root", "");
+	}
+	catch(Exception e){
+	System.out.println("something fishy!");
+	}
+
+        DataService dataService = new DataService();
+        System.out.println( "Number of terms : " + dataService.getAllTerm().size());
+        dataService.saveTerm("Term from app");
+    }
+}
